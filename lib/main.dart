788809da7d277d03faae603443c7d31878989808
lmtvt <luca.mathevet@epitech.eu>
2020-03-22 12:37:48 +0100

    Homepage DONE

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-//import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -31,15 +32,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -47,68 +39,76 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline,
-            ),
-          ],
+      body: Column(children: <Widget>[
+        SizedBox(
+          height: MediaQuery.of(context).size.height / 2,
+          child: Container(
+            color: Colors.white,
+            child: Center(
+                child: Padding(
+              padding: const EdgeInsets.all(40.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Face à l’épidémie du Covid-19 qui touche notre pays, les soignants, les pharmaciens, les policiers, les pompiers et tout les travailleurs qui risquent leurs santé en se battant au jour le jour contre l’épidémie et en assurant les fonctions vitales de notre pays ont plus que besoin de solidarité et de soutien, certains citoyens proposent des services et/ou des aides matérielles afin de soulager leurs quotidien.',
+                    textAlign: TextAlign.center,
+                  ),
+                  Text(
+                      'Cette plateforme a pour but de les diffuser en mettant en place un réseaux d’entraide.',
+                      textAlign: TextAlign.center)
+                ],
+              ),
+            )),
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        SizedBox(
+          height: MediaQuery.of(context).size.height / 2,
+          child: Container(
+            color: Colors.blue,
+            child: Row(children: <Widget>[
+              SizedBox(
+                width: MediaQuery.of(context).size.width / 2,
+                child: Container(
+                  color: Color(0xFFB2C8E9),
+                  child: mycontent('''Je suis 
+un professionel de santé, de sécurité,
+un travailleur qui assure les fonctions vitales
+de notre pays.''', Colors.black),
+                ),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width / 2,
+                child: Container(color: Colors.black,
+                child: mycontent('''Je suis
+un particulier, une entreprise,
+un citoyen qui souhaite apporter son aide.''', Colors.white),),
+              )
+            ]),
+          ),
+        )
+      ]),
     );
   }
+}
+
+Widget mycontent(String text, Color color) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: <Widget>[
+      Text(
+        text,
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 20, color: color),
+      ),
+      Spacer(),
+      Align(
+        alignment: Alignment.bottomRight,
+        child: Icon(Icons.arrow_forward, size: 250, color: color,),
+      ),
+    ],
+  );
 }
